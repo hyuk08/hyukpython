96e746275d188fee7f468fec1dfc9817b57c79bd
seunghyuk <cmo020824@gmail.com>
2024-04-23 16:43:26 +0900

    0423_1643

--- 크리에이티브 컴퓨팅/p5js.js
+++ 크리에이티브 컴퓨팅/p5js.js
@@ -4,34 +4,35 @@ let cols, rows;
 function setup() {
   createCanvas(400, 400);
   background(0);
-  cols = width/unit;
-  rows = height/unit;
+  cols = width / unit;
+  rows = height / unit;
 }
+
 
 function draw() {
   background(220);
   randomSeed();
-  
-  for (let i=0; i<cols; i++) {
-    for (let j=0; j<rows; j++) {
-    
-      let cx = i*unit + unit/2;
-      let cy = j*unit + unit/2;
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+
+      let cx = i * unit + unit / 2;
+      let cy = j * unit + unit / 2;
 
       let p = random();
       if (p > 0.9) {
-        drawTarget(cx, cy, unit, random(2, mouseY/5));
+        drawTarget(cx, cy, unit, random(2, mouseY / 5));
       } else if (p > 0.1) {
         eye(cx, cy, unit, random(255), 3, keyIsPressed)
-        } else if (mouseIsPressed) {
+      } else if (mouseIsPressed) {
         fill(243, 138, 255)
         //아직 안 채웠음 ㅋㅋ
-          } else {
-          fill(0, 35, 232);
-          stroke(243, 138, 255)
-          circle(cx, cy, unit)
-          }
-        
+      } else {
+        fill(0, 35, 232);
+        stroke(243, 138, 255)
+        circle(cx, cy, unit)
+      }
+
       // circle(cx, cy, unit);
       // drawTarget(cx, cy, unit, random(2, mouseY/5));
       // let wink = random([true, false]);
@@ -49,10 +50,10 @@ function eye(x, y, d, fc/*fillcolor*/, thickness, wink) {
   circle(x, y, d)
 
   if (wink) {
-      line(x - d/2, y, x + d/2, y);
+    line(x - d / 2, y, x + d / 2, y);
   } else {
     // fill (fc - 30);
-    circle(x, y, d/2);
+    circle(x, y, d / 2);
   }
 }
 
--- 크리에이티브 컴퓨팅/pong.js
+++ 크리에이티브 컴퓨팅/pong.js
@@ -1,72 +1,72 @@
-let bx, by; 
-let vx, vy; 
-let bw; 
-let barX, barY; 
+let bx, by;
+let vx, vy;
+let bw;
+let barX, barY;
 
-let imageFile = "1.jpeg"; 
+let imageFile = "1.jpeg";
 
-let imageLoaded = false; 
-let loadedImage; 
-let imageOpacity = 255; 
+let imageLoaded = false;
+let loadedImage;
+let imageOpacity = 255;
 
-let audio = new Audio('vine_boom.mp3'); 
-audio.volume = 0.1; 
-let audio1 = new Audio('999.mp3'); 
+let audio = new Audio('vine_boom.mp3');
+audio.volume = 0.1;
+let audio1 = new Audio('999.mp3');
 audio1.volume = 0.05;
 
 function preload() {
-  
+
   loadedImage = loadImage(imageFile, img => {
     imageLoaded = true;
   });
 }
 
 function setup() {
-  createCanvas(400, 300); 
-  
-  bx = width/2; 
-  by = height/2; 
-  bw = 10; 
+  createCanvas(400, 300);
+
+  bx = width / 2;
+  by = height / 2;
+  bw = 10;
   vx = 10;
   vy = random(0, 5);
-  barX = 360; 
-  barY = 120; 
+  barX = 360;
+  barY = 120;
 }
 
 function draw() {
-  background(0); 
+  background(0);
   rect(bx, by, bw, bw);
   bx += vx;
   by += vy;
-  
+
   if (bx < 0 || width < bx + bw) {
     vx *= -1;
   } else if (by < 0 || height < by + bw) {
     vy *= -1;
   }
-  
-  rect(barX, barY, bw/2, bw*4);
+
+  rect(barX, barY, bw / 2, bw * 4);
   if (keyIsDown(DOWN_ARROW)) {
     barY += 6;
   }
-  
+
   if (keyIsDown(UP_ARROW)) {
     barY -= 6;
   }
-  
+
   if (bx + bw > barX) {
-    if (by + bw > barY && by < barY + bw*4) {
-      vx *= -1; 
-      fill(random(0, 255), random(0, 255), random(0, 255)); 
-      audio.play(); 
-      setTimeout(function() {
+    if (by + bw > barY && by < barY + bw * 4) {
+      vx *= -1;
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      audio.play();
+      setTimeout(function () {
         audio.pause();
         audio.currentTime = 0;
-      }, 1500); 
-     
+      }, 1500);
+
       if (imageLoaded) {
         imageMode(CENTER);
-        image(loadedImage, width/2, height/2);
+        image(loadedImage, width / 2, height / 2);
         imageOpacity -= 2;
         tint(255, imageOpacity);
       }
@@ -74,7 +74,7 @@ function draw() {
   }
   if (barY < 0 || barY + bw * 4 > height) {
     audio1.play();
-    setTimeout(function() {
+    setTimeout(function () {
       audio1.pause();
       audio1.currentTime = 0;
     }, 1500);
