df51bce0950c795e84e238f20e00d476710e801a
hyuk08 <cmo020824@gmail.com>
2024-06-03 23:55:41 +0900

    0603

---
+++ 0603_test.py
@@ -0,0 +1,39 @@
+import time
+
+count = 149180
+
+for i in range(2):
+    
+    count += 1
+    print(f'오늘 사망자 수는 {count} 명 입니다.')
+
+    time.sleep(0.7)
+
+for i in range(4):
+    
+    count += 1
+    print(f'오늘 사망자 수는 {count} 명 입니다.')
+
+    time.sleep(0.1)
+
+
+for i in range(2):
+    
+    count += 1
+    print(f'오늘 사망자 수는 {count} 명 입니다.')
+
+    time.sleep(1)
+
+for i in range(10):
+    
+    count += 1
+    print(f'오늘 사망자 수는 {count} 명 입니다.')
+
+    time.sleep(0.1)
+
+for i in range(60):
+    
+    count += 1
+    print(f'오늘 사망자 수는 {count} 명 입니다.')
+
+    time.sleep(0.5)
